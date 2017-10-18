fa11e7493c756e7205d9dae6a8a63fa80fd106c7
HDO <hungdohuy@gmail.com>
2017-10-18 18:33:10 +0700

    Add validator javascript for login page

--- js/validator.js
+++ js/validator.js
@@ -1,13 +1,15 @@
 function validate_login() {
     var email = document.forms["loginform"]["username"].value;
     var password = document.forms["loginform"]["password"].value;
-    var re_email = new RegExp('^\w+([\.-_]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})$');
-    var re_password = new RegExp('^.*{6,}$');
+    var re_email = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})$/;
+    var re_password = /(?=.*[a-zA-Z])(?=.*[0-9])(?=.{6,})/;
 
-    if (re_email.test(email)) {
-    	return true;
-    } else {
-        alert("Email or password is not valid");
+    if ((re_email.test(email)) && (re_password.test(password))) {
+    	console.log('Valid');
         return true;
+    } else {
+        console.log('Not valid');
+        alert("Email or password is not valid");
+        return false;
     }
 }
